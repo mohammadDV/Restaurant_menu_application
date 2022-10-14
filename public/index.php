c0ae6e5098c18e7afb4e00622ac377ce7998d3f2
MohammadDV <sasha.daneshmand@gmail.com>
2022-10-14 14:58:26 +0300

    make it better9

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers:*");
+
+if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {//send back preflight request response
+return "";
+}
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
